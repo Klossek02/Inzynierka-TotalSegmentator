fc06538bf520faec20b4006b622216f6fab05351
Aleksandra Kłos <67869082+Klossek02@users.noreply.github.com>
2024-12-03 09:04:45 +0100

    adding comments

--- test_application.py
+++ test_application.py
@@ -23,9 +23,11 @@ from segmentation import preprocess_img, segment_img
 from model import get_unet_model
 from visualization import display_single_slice
 
-# All tests have been written preserving TDD principles
+# all tests have been written preserving TDD principles
 
-class TestDataImport(unittest.TestCase):
+class TestDataImport(unittest.TestCase): # class for testing data imports
+
+    # checks whether function 'validate_nifti'correctly identifies a valid NIfTI file 
     def test_validate_nifti_valid(self):
 
         # Given 
@@ -43,12 +45,13 @@ class TestDataImport(unittest.TestCase):
 
         os.remove(temp_file)
 
+    # checks whether function 'validate_nifti' correctly identifies an invalid NIfTI file (e.g. text file/ CSV file etc.)
     def test_validate_nifti_invalid(self):
 
         # Given
         temp_file = 'temp_invalid_nifti.txt'
         with open(temp_file, 'w') as f:
-            f.write('ERROR: not a NIfTI file.')
+            f.write('Not a NIfTI file.')
 
         # When 
         result = data_import.validate_nifti(temp_file)
@@ -58,6 +61,8 @@ class TestDataImport(unittest.TestCase):
 
         os.remove(temp_file)
 
+    # checks whether function 'load_nifti' correctly loads data and affine matrix from a NIfTI file
+    # affine matrix reference: https://medium.com/@junfeng142857/affine-transformation-why-3d-matrix-for-a-2d-transformation-8922b08bce75
     def test_load_nifti_valid(self):
 
         # Given
@@ -76,12 +81,13 @@ class TestDataImport(unittest.TestCase):
 
         os.remove(temp_file)
 
+    # checks the response of 'load_nifti' to invalid NIfTI files
     def test_load_nifti_invalid(self):
 
         # Given
         temp_file = 'temp_invalid_nifti.txt'
         with open(temp_file, 'w') as f:
-            f.write('This is not a NIfTI file')
+            f.write('Invalid NIfTI file.')
 
         # When, Then
         with self.assertRaises(ValueError): 
@@ -89,13 +95,14 @@ class TestDataImport(unittest.TestCase):
 
         os.remove(temp_file)
 
-class TestVisualization(unittest.TestCase):
+class TestVisualization(unittest.TestCase): # class for testing visualization aspects
     def setUp(self):
         if QApplication.instance() is None:
             self.app = QApplication(sys.argv)
         else:
             self.app = QApplication.instance()
 
+    # checks whether function 'convert_img_slices' correctly splits a NIfTI file into scans in sagittal, coronal, axial axis (planes)
     def test_convert_img_slices(self):
 
         # Given
@@ -115,12 +122,13 @@ class TestVisualization(unittest.TestCase):
 
         os.remove(temp_file)
 
+    # checks the response of 'convert_img_slices' to invalid NIfTI files
     def test_convert_img_slices_invalid_file(self):
 
         # Given
         temp_file = 'temp_invalid_nifti.txt'
         with open(temp_file, 'w') as f:
-            f.write('This is not a NIfTI file')
+            f.write('Invalid NIfTI file.')
 
         # When, Then
         with self.assertRaises(ValueError):
@@ -128,6 +136,7 @@ class TestVisualization(unittest.TestCase):
 
         os.remove(temp_file)
 
+    # checks whether function 'display_single_slice' correctly displays a single slice of a NIfTI file in a QLabel widget
     def test_display_single_slice(self):
 
         # Given
@@ -145,7 +154,9 @@ class TestVisualization(unittest.TestCase):
         # Then
         self.assertIsNotNone(label.pixmap())
 
-class TestSegmentation(unittest.TestCase):
+class TestSegmentation(unittest.TestCase): # class for testing segmentation aspects
+
+    # checks whether function 'preprocess_img' correctly preprocesses a 3D numpy array into a torch tensor 
     def test_preprocess_img(self):
 
         # Given
@@ -158,6 +169,7 @@ class TestSegmentation(unittest.TestCase):
         self.assertEqual(img_tensor.shape, (1, 128, 128, 128))
         self.assertTrue(isinstance(img_tensor, torch.Tensor))
 
+    # checks whether function 'segment_img' correctly segments a 3D numpy array using a trained U-Net model
     def test_segment_img(self):
 
         # Given
@@ -171,6 +183,8 @@ class TestSegmentation(unittest.TestCase):
         self.assertEqual(seg_pred.shape, (128, 128, 128))
         self.assertTrue(isinstance(seg_pred, np.ndarray))
 
+
+    # checks whether function 'save_segmentation' correctly saves a 3D numpy array as a NIfTI file
     def test_save_segmentation(self):
 
         # Given
@@ -188,7 +202,9 @@ class TestSegmentation(unittest.TestCase):
 
         os.remove(save_path)
 
-class TestModel(unittest.TestCase):
+class TestModel(unittest.TestCase): # class for testing model abilities 
+
+    # checks whether function 'get_unet_model' correctly returns a U-Net model with specified number of classes and input channels (specified parameters)
     def test_get_unet_model(self):
 
         # Given
@@ -205,12 +221,14 @@ class TestModel(unittest.TestCase):
         from monai.networks.nets import UNet
         self.assertIsInstance(net, UNet)
 
+    # checks whether function 'get_unet_model' correctly raises an exception when invalid parameters are provided (negative number of classes or input channels)
     def test_get_unet_model_invalid_params(self):
         # Given, When, Then
         with self.assertRaises(Exception):
             model.get_unet_model(num_classes=-1, in_channels=1) # since the function doesn't have input validation, error must come within UNet
 
 
+    # checks whether function 'dice_metric' correctly computes the Dice metric (the metrics must be calculated in the range [0,1])
     def test_dice_metric(self):
         # Given
         from monai.metrics import DiceMetric
@@ -222,7 +240,9 @@ class TestModel(unittest.TestCase):
         # Then
         self.assertTrue(0 <= dice.item() <= 1)
 
-class TestDemo(unittest.TestCase):
+class TestDemo(unittest.TestCase): # class for testing the 'demo.py' class functionality
+
+    # checks whether function 'convert_to_stl' correctly converts a 3D numpy array into a STL file, hence generates it afterwards
     def test_convert_to_stl(self):
 
         # Given
@@ -241,6 +261,8 @@ class TestDemo(unittest.TestCase):
 
         os.remove(out_path)
 
+    # checks whether function 'convert_to_stl' correctly raises an exception when the input data is empty 
+    # in other words, it doesn't generate an STL file if data is empty
     def test_convert_to_stl_empty_data(self):
 
         # Given
@@ -253,11 +275,12 @@ class TestDemo(unittest.TestCase):
         # Then
         self.assertFalse(os.path.exists(out_path))
 
-class TestMedicalImageViewer(unittest.TestCase):
+class TestGUI(unittest.TestCase): # class for testing GUI functionality
     def setUp(self):
         self.app = QApplication.instance() or QApplication(sys.argv)
         self.viewer = MedicalImageViewer()
 
+    # checks whether logs error messages in the GUI widget are visible
     def test_log_message(self):
 
         # Given
@@ -269,6 +292,7 @@ class TestMedicalImageViewer(unittest.TestCase):
         # Then
         self.viewer.error_log.appendPlainText.assert_called_with("Test message")
 
+    # checks whether function 'get_distinct_colors' correctly returns a list of distinct colors
     def test_get_distinct_colors(self):
 
         # Given
@@ -280,6 +304,7 @@ class TestMedicalImageViewer(unittest.TestCase):
         # Then
         self.assertEqual(len(colors), N)
 
+    # checks whether function 'init_ui' correctly sets up the user interface
     def test_init_ui(self):
         # Given, When
         try:
@@ -288,8 +313,9 @@ class TestMedicalImageViewer(unittest.TestCase):
             # Then
             self.fail(f"init_ui raised an exception: {e}")
 
+    # checks whether function 'setup_menu_bar' correctly sets up the menu bar with expected actions
     def test_setup_menu_bar(self):
-
+ 
         # Given, When
         self.viewer.setup_menu_bar()
 
@@ -301,18 +327,8 @@ class TestMedicalImageViewer(unittest.TestCase):
         self.assertIn('Save segmentation', actions)
         self.assertIn('Close segmentation', actions)
 
-    def test_on_upload_data_cancelled(self):
-        # Given
-        with patch('PyQt5.QtWidgets.QInputDialog.getItem', return_value=('', False)):
-            # When
-            self.viewer.on_upload_data()
-            # Then
-            with patch.object(self.viewer, 'on_upload_ct_scan') as mock_upload_ct, \
-                 patch.object(self.viewer, 'on_upload_segmented_ct_scan') as mock_upload_seg:
-                self.viewer.on_upload_data()
-                mock_upload_ct.assert_not_called()
-                mock_upload_seg.assert_not_called()
 
+    # checks whether function 'on_upload_data' correctly handles the upload data action
     def test_on_upload_data_upload_ct_scan(self):
 
         # Given
@@ -325,6 +341,21 @@ class TestMedicalImageViewer(unittest.TestCase):
             # Then
             mock_on_upload_ct_scan.assert_called_once()
 
+
+    # checks whether function 'on_upload_data' correctly handles the upload data action when user decides to cancel the upload operation
+    def test_on_upload_data_cancelled(self):
+        # Given
+        with patch('PyQt5.QtWidgets.QInputDialog.getItem', return_value=('', False)):
+            # When
+            self.viewer.on_upload_data()
+            # Then
+            with patch.object(self.viewer, 'on_upload_ct_scan') as mock_upload_ct, \
+                 patch.object(self.viewer, 'on_upload_segmented_ct_scan') as mock_upload_seg:
+                self.viewer.on_upload_data()
+                mock_upload_ct.assert_not_called()
+                mock_upload_seg.assert_not_called()
+
+    # checks whether function 'on_upload_ct_scan' correctly handles the upload CT scan action
     def test_on_upload_ct_scan_valid_file(self):
 
         # Given
@@ -343,6 +374,7 @@ class TestMedicalImageViewer(unittest.TestCase):
             # Then
             self.assertTrue(self.viewer.segment_action.isEnabled())
 
+    # checks whether the function 'on_upload_ct_scan' correctly handles an invalid CT scan file
     def test_on_upload_ct_scan_invalid_file(self):
 
         # Given
@@ -357,6 +389,7 @@ class TestMedicalImageViewer(unittest.TestCase):
             # Then
             mock_critical.assert_called()
 
+    # checks whether the function 'on_upload_segmented_ct_scan' correctly handles the upload of a valid segmentation file, for already segmented data
     def test_on_upload_segmented_ct_scan(self):
 
         # Given
@@ -373,6 +406,8 @@ class TestMedicalImageViewer(unittest.TestCase):
             self.assertIsNotNone(self.viewer.segmentation_result)
             self.viewer.render_3d_visualization_from_data.assert_called()
 
+
+    # checks whether function 'on_segment_image' reacts to the the lack of uploaded segmentation data
     def test_on_segment_image_no_ct_scans(self):
 
         # Given
@@ -387,6 +422,7 @@ class TestMedicalImageViewer(unittest.TestCase):
             args = mock_warning.call_args[0]
             self.assertIn("Please upload your CT scan", args[2])
 
+    # checks whether function 'on_segment_image' correctly handles the uploaded segmentation data
     def test_on_segment_image_with_ct_scans(self):
 
         # Given
@@ -403,6 +439,7 @@ class TestMedicalImageViewer(unittest.TestCase):
             # Then
             self.assertIsNotNone(self.viewer.segmentation_result)
 
+    # checks whether function 'load_segmentation_model' correctly loads the segmentation model
     def test_load_segmentation_model(self):
 
         # Given
@@ -415,6 +452,7 @@ class TestMedicalImageViewer(unittest.TestCase):
             # Then
             self.assertIsNotNone(net)
 
+    # checks whether function 'load_segmentation_model' correctly handles the case when the model file is not found
     def test_load_segmentation_model_file_not_found(self):
 
         # Given 
@@ -431,6 +469,7 @@ class TestMedicalImageViewer(unittest.TestCase):
             mock_critical.assert_called()
             mock_log_message.assert_called_with("ERROR: best_metric_model.pth not found.")
 
+    # checks whether function 'convert_img_slices' correctly converts the 3D image into slices for visualization, and whether the images are correctly updated
     def test_update_image_placeholders(self):
 
         # Given
@@ -449,6 +488,7 @@ class TestMedicalImageViewer(unittest.TestCase):
         self.assertIsNotNone(self.viewer.scan_top_right.pixmap())
         self.assertIsNotNone(self.viewer.scan_bottom_left.pixmap())
 
+    # checks whether function 'on_save_segmentation' correctly handles the saving of a segmentation result
     def test_on_save_segmentation_with_data(self):
 
         # Given
@@ -466,6 +506,7 @@ class TestMedicalImageViewer(unittest.TestCase):
             args = mock_info.call_args[0]
             self.assertIn("Segmentation has been saved at", args[2])
 
+    # checks whether function 'on_save_segmentation' correctly handles the case when there is no segmentation data to save
     def test_on_save_segmentation_no_data(self):
 
         # Given
@@ -480,6 +521,7 @@ class TestMedicalImageViewer(unittest.TestCase):
             args = mock_info.call_args[0]
             self.assertIn("There is no segmentation data to save.", args[2])
 
+    # checks whether function 'on_close_segmentation' correctly handles the closing of the segmentation window
     def test_on_close_segmentation(self):
 
         # Given
@@ -493,6 +535,7 @@ class TestMedicalImageViewer(unittest.TestCase):
         self.assertIsNone(self.viewer.ct_scans)
         self.assertIsNone(self.viewer.segmentation_result)
 
+    # checks whether function 'on_zoom_in' correctly handles the zoom in functionality
     def test_on_zoom_in(self):
         # Given
         self.viewer.plotter = MagicMock()
@@ -506,6 +549,7 @@ class TestMedicalImageViewer(unittest.TestCase):
         self.viewer.plotter.render.assert_called()
         self.viewer.vtk_widget.update.assert_called()
 
+    # checks whether function 'on_zoom_out' correctly handles the zoom out functionality    
     def test_on_zoom_out(self):
 
         # Given
@@ -520,6 +564,7 @@ class TestMedicalImageViewer(unittest.TestCase):
         self.viewer.plotter.render.assert_called()
         self.viewer.vtk_widget.update.assert_called()
 
+    # checks whether function 'on_help' correctly handles the help functionality
     def test_on_help(self):
 
         # Given, When
@@ -531,6 +576,7 @@ class TestMedicalImageViewer(unittest.TestCase):
             args = mock_info.call_args[0]
             self.assertIn("SegMed help information:", args[2])
 
+    # checks whether function 'on_report_problem' correctly handles the report problem functionality
     def test_on_report_problem(self):
 
         # Given, When
@@ -542,6 +588,7 @@ class TestMedicalImageViewer(unittest.TestCase):
             args = mock_info.call_args[0]
             self.assertIn("Please provide us with the details of the problem you encountered.", args[2])
 
+    # checks whether function 'on_about' correctly handles the about message box
     def test_on_about(self):
 
         # Given, When
@@ -551,6 +598,7 @@ class TestMedicalImageViewer(unittest.TestCase):
             # Then
             mock_exec.assert_called()
 
+    # checks whether function 'on_slider_move' correctly handles the slider movement
     def test_on_slider_move(self):
 
         # Given
@@ -575,6 +623,7 @@ class TestMedicalImageViewer(unittest.TestCase):
             # Then
             self.fail(f"on_slider_move raised an exception: {e}")
 
+    # checks whether function 'render_3d_visualization_from_data' correctly renders the 3D visualization from segmentation data
     def test_render_3d_visualization_from_data(self):
 
         # Given
@@ -594,7 +643,9 @@ class TestMedicalImageViewer(unittest.TestCase):
             mock_plotter_instance.show.assert_called()
             self.viewer.vtk_widget.update.assert_called()
 
-class TestPerformance(unittest.TestCase):
+class TestPerformance(unittest.TestCase): # class for testing program performance
+
+    # checks the operation of the full processing path: preprocessing, segmentation, saving results
     def test_full_pipeline(self):
         # Given
         ct_scan = np.random.rand(128, 128, 128)
@@ -614,6 +665,7 @@ class TestPerformance(unittest.TestCase):
         os.remove("temp_output.nii.gz")
 
 
+    # checks the speed of preprocessing, segmentation and verifies whether they are within the limits specified in non-functional requirements
     def test_segmentation_speed(self):
         # Given
         ct_scan = np.random.rand(128, 128, 128).astype(np.float32)
@@ -637,7 +689,7 @@ class TestPerformance(unittest.TestCase):
         assert preprocess_time < 5.0, "Preprocessing time exceeded 5 seconds"
         assert segmentation_time < 10.0, "Segmentation time exceeded 10 seconds"
 
-
+# checks the speed of visualization (starting, displaying slices in each axis/ ready 3D segmentation) and verifies whether it is within the limits specified in non-functional requirements
 def test_visualization_speed():
     # Given
     ct_slice = np.random.randint(0, 255, size=(128, 128)).astype(np.uint8)

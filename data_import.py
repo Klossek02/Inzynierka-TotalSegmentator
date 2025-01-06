849ec1a2386363f3463c15af268d87123946fc78
Aleksandra Kłos <67869082+Klossek02@users.noreply.github.com>
2025-01-06 23:12:41 +0000

    added rotation

--- data_import.py
+++ data_import.py
@@ -1,6 +1,7 @@
 # === data_import.py ===
 
 import nibabel as nib
+import numpy as np
 
 def validate_nifti(file_path):
     try:
@@ -17,3 +18,27 @@ def load_nifti(file_path):
         return data, affine
     except Exception as e:
         raise ValueError(f"Failed to load NIfTI file {file_path}: {e}")
+
+
+def get_scan_orientation(affine):
+    return nib.orientations.io_orientation(affine)
+
+
+def reorient_scan(data, affine, desired_ornt =('R', 'A', 'S'), logger=None):
+    try:
+        current_ornt = get_scan_orientation(affine)
+        desired_ornt_matrix = nib.orientations.axcodes2ornt(desired_ornt)
+        transform = nib.orientations.ornt_transform(current_ornt, desired_ornt_matrix)
+        reoriented_data = nib.orientations.apply_orientation(data, transform)
+        new_affine = affine.copy()
+        new_affine = nib.orientations.inv_ornt_aff(transform, data.shape)
+        new_affine = np.dot(new_affine, affine)
+        
+        if logger:
+            logger("Scan data has been reoriented to the desired orientation.")
+        
+        return reoriented_data, new_affine
+    except Exception as e:
+        if logger:
+            logger(f"Error in reorient_scan: {e}")
+        raise
